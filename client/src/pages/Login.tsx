f54d075a6560bd23d51ab6e66e907a86c17badd1
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-01 22:34:09 +0200

    login : redirection + update auth state + manage error

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,14 +1,18 @@
 import { Formik } from "formik";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Button, Form, Header, Message } from "semantic-ui-react";
 import Layout from "../components/Layout";
+import { ACTION, useStore } from "../components/Store";
 import { LoginSchema, LoginValues } from "../schemas/loginSchema";
 import authService from "../services/authService";
+import { translateMessage } from "../utils/responseMessage";
 import { MessageType } from "../utils/types";
 
 const Login: React.FC = () => {
-  //   const router = useRouter();
-  const [message, setMessage] = React.useState<MessageType>();
+  let history = useHistory();
+  const [message, setMessage] = React.useState<MessageType>({});
+  const [, dispatch] = useStore();
 
   const initialValues: LoginValues = {
     email: "",
@@ -23,7 +27,20 @@ const Login: React.FC = () => {
     }
   }, [message]);
 
-  // console.log(document.cookie.split(";"));
+  const handleSubmit = async (values: LoginValues) => {
+    const response = await authService.login(values);
+
+    if (response.ok) {
+      dispatch({ type: ACTION.LOG_IN });
+      history.push("/");
+    } else {
+      const jsonResponse = await response.json();
+      setMessage({
+        color: "red",
+        text: translateMessage(jsonResponse.message, response.ok),
+      });
+    }
+  };
 
   return (
     <Layout>
@@ -34,39 +51,13 @@ const Login: React.FC = () => {
           validateOnBlur={false}
           validateOnChange={false}
           validationSchema={LoginSchema}
-          onSubmit={async (values, { setErrors }) => {
-            console.log(
-              await (
-                await authService.login(values.email, values.password)
-              ).json()
-            );
-
-            // const response = await authService.login(
-            //   values.email,
-            //   values.password
-            // );
-
-            // const { data, error } = await login(values);
-
-            // if (data?.login.errors) {
-            //   setErrors(toErrorMap(data.login.errors));
-            // }
-
-            // if (data?.login.login) {
-            //   setAccessToken(data.login.login.accessToken);
-            //   setRefreshToken(data.login.login.refreshToken);
-
-            //   if (typeof router.query.next === "string") {
-            //     router.push(router.query.next);
-            //   } else {
-            //     router.push("/");
-            //   }
-            // }
-          }}
+          onSubmit={handleSubmit}
         >
           {({ values, handleChange, handleSubmit, isSubmitting, errors }) => (
             <Form onSubmit={handleSubmit}>
-              {message && <Message color="red">{message}</Message>}
+              {message.color && (
+                <Message color={message.color}>{message.text}</Message>
+              )}
               <Form.Input
                 id="email"
                 icon="at"
