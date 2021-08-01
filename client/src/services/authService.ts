7a3d45174b7454a0d43f024146c218a7961601aa
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-01 22:30:09 +0200

    add auth service : login and get new access token

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,16 +1,36 @@
-const login = async (email: string, password: string) => {
-  console.log(JSON.stringify({ email, password }));
-  return await fetch(process.env.REACT_APP_API_URL + "/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
+import { LoginValues } from "../schemas/loginSchema";
+import { fetchData, fetchManager } from "./globalService";
+
+const login = (login: LoginValues): Promise<Response> => {
+  return fetchManager({
+    url: process.env.REACT_APP_API_URL + "/auth/login",
+    options: {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify(login),
+  });
+};
+
+const getNewAccessToken = (): Promise<Response> => {
+  return fetchData({
+    url: process.env.REACT_APP_API_URL + "/auth/refresh-token",
+    options: {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
   });
 };
 
 const authService = {
   login,
+  getNewAccessToken,
 };
 
 export default authService;
