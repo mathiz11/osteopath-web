9fe5a9ec08177f6aca9fb5df8d65eb09590d7ed4
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-01 22:31:24 +0200

    change login schema

--- client/src/schemas/loginSchema.ts
+++ client/src/schemas/loginSchema.ts
@@ -7,9 +7,11 @@ export type LoginValues = {
 
 export const LoginSchema = Yup.object().shape({
   email: Yup.string()
+    .email("L'email n'est pas au bon format")
     .max(100, "L'email est trop long")
     .required("Ce champ est obligatoire"),
   password: Yup.string()
+    .min(8, "Le mot de passe doit contenir au moins 8 caractères")
     .max(100, "Le mot de passe est trop long")
     .required("Ce champ est obligatoire"),
 });
