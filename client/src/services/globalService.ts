e7985f691c7b698e839b52d4b09588f5d62c7241
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-01 22:30:49 +0200

    add globalservice to manage jwt too

---
+++ client/src/services/globalService.ts
@@ -0,0 +1,39 @@
+import authService from "./authService";
+
+type FetchOptions = {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  credentials?: RequestCredentials;
+  headers?: HeadersInit;
+};
+
+export type FetchParams = {
+  url: string;
+  options: FetchOptions;
+  body?: BodyInit;
+};
+
+export const fetchData = ({
+  url,
+  options,
+  body,
+}: FetchParams): Promise<Response> => {
+  return fetch(url, { ...options, body });
+};
+
+export const fetchManager = async (
+  fetchParams: FetchParams
+): Promise<Response> => {
+  let response = await fetchData(fetchParams);
+
+  if (response.status === 401) {
+    const responseNewAccessToken = await authService.getNewAccessToken();
+
+    if (responseNewAccessToken.status === 401) {
+      window.location.pathname = "/login";
+    } else if (responseNewAccessToken.ok) {
+      response = await fetchData(fetchParams);
+    }
+  }
+
+  return response;
+};
