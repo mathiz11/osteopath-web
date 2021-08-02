c83a6fcd8b51c1e8f62c291e43f55f32d8fc0ba6
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-02 22:25:01 +0200

    update app style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Store } from "./components/Store";
 import Routes from "./components/Routes";
 
 import "./App.css";
-import "semantic-ui-css/semantic.min.css";
 
 const App = () => (
   <Store>
