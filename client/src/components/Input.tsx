0229014b024b3bc815e9477e7660c92c65a133d4
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-03 18:26:24 +0200

    add required input and text area support

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -4,11 +4,13 @@ import "../styles/Input.css";
 type InputProps = {
   value: any;
   id: string;
-  placeholder: string;
+  placeholder?: string;
   label: string;
   type?: string;
   onChange: (e: ChangeEvent<any>) => void;
   error?: string;
+  isRequired?: boolean;
+  isTextArea?: boolean;
 };
 
 const Input = ({
@@ -19,17 +21,23 @@ const Input = ({
   type,
   onChange,
   error,
+  isRequired,
+  isTextArea,
 }: InputProps) => {
   return (
     <div className={`form-control${error ? " error" : ""}`}>
-      <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        placeholder={placeholder}
-        type={type ? type : "text"}
-        value={value}
-        onChange={onChange}
-      />
+      <label htmlFor={id}>{`${label}${isRequired ? " *" : ""}`}</label>
+      {isTextArea ? (
+        <textarea id={id} onChange={onChange} value={value} rows={3} />
+      ) : (
+        <input
+          id={id}
+          placeholder={placeholder}
+          type={type ? type : "text"}
+          value={value}
+          onChange={onChange}
+        />
+      )}
       <span className="form-error">{error}</span>
     </div>
   );
