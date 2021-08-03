9b8602b2e1a1718b008450aa9ec381c7c43fe830
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-03 20:42:33 +0200

    add client informations

--- client/src/pages/ClientPage.tsx
+++ client/src/pages/ClientPage.tsx
@@ -3,6 +3,8 @@ import clientService from "../services/clientService";
 import { Client } from "../entities/Client";
 import Layout from "../components/Layout";
 import { FaPlus } from "react-icons/fa";
+import { BsFillEnvelopeFill, BsGeo, BsPhone } from "react-icons/bs";
+import { HiDotsVertical } from "react-icons/hi";
 import "../styles/ClientPage.css";
 import AnimalList from "../components/AnimalList";
 
@@ -22,21 +24,46 @@ const ClientPage = ({ match }: any) => {
   return (
     <Layout>
       <div className="client">
-        {client && (
-          <>
-            <div className="header add-button">
-              <h1>
-                {client.firstname} {client.lastname}
-              </h1>
-              <button className="primary" onClick={() => {}}>
-                <FaPlus />
-                <span>Ajouter</span>
-              </button>
-            </div>
-            <h2>Animaux</h2>
-            <AnimalList animals={client.animals} />
-          </>
-        )}
+        <div className="container">
+          {client && (
+            <>
+              <div className="client-info">
+                <div className="client-info-name">
+                  <span>{client.firstname}</span>
+                  <span>{client.lastname}</span>
+                </div>
+                <div className="client-info-detail">
+                  <div className="client-info-detail-item">
+                    <BsFillEnvelopeFill />
+                    <span>{client.email}</span>
+                  </div>
+                  <div className="client-info-detail-item">
+                    <BsPhone />
+                    <span>{client.phone}</span>
+                  </div>
+                  <div className="client-info-detail-item">
+                    <BsGeo />
+                    <span>{client.address}</span>
+                  </div>
+                </div>
+                <div className="client-info-button">
+                  <button className="circle pure">
+                    <HiDotsVertical size="18" />
+                  </button>
+                </div>
+              </div>
+
+              <div className="header add-button">
+                <h2>Animaux</h2>
+                <button className="primary" onClick={() => {}}>
+                  <FaPlus />
+                  <span>Ajouter</span>
+                </button>
+              </div>
+              <AnimalList animals={client.animals} />
+            </>
+          )}
+        </div>
       </div>
     </Layout>
   );
