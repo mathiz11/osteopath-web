3d06af4d6bf686d2ebe2d3a545257f2d94f1703f
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-03 18:27:16 +0200

    add client schema

---
+++ client/src/schemas/clientSchema.ts
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+
+export type ClientValues = {
+  firstname?: string;
+  lastname?: string;
+  phone?: string;
+  email?: string;
+  address?: string;
+};
+
+export const ClientSchema = Yup.object().shape({
+  firstname: Yup.string()
+    .max(100, "Le prénom est trop long")
+    .required("Ce champ est obligatoire"),
+  lastname: Yup.string()
+    .max(100, "Le nom est trop long")
+    .required("Ce champ est obligatoire"),
+  phone: Yup.string()
+    .matches(/^[0-9]{10}$/, "Le numéro de téléphone est invalide")
+    .required("Ce champ est obligatoire"),
+  email: Yup.string()
+    .email("L'email est invalide")
+    .required("Ce champ est obligatoire"),
+  address: Yup.string().required("Ce champ est obligatoire"),
+});
