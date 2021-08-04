506c89ee35009576f1b19e81e1fe487579ff7e6b
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-04 12:39:30 +0200

    add edit client + optimize in  home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,49 +1,30 @@
-import React, { useState } from "react";
-import Layout from "../components/Layout";
-import clientService from "../services/clientService";
-import { FaPlus } from "react-icons/fa";
-import { Client } from "../entities/Client";
-import "../styles/HomePage.css";
-import Modal from "../components/Modal";
-import { MessageType } from "../utils/types";
-import { ClientSchema, ClientValues } from "../schemas/clientSchema";
 import { Formik } from "formik";
-import Message from "../components/Message";
-import Input from "../components/Input";
+import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 import ClientList from "../components/ClientList";
-import { useMemo } from "react";
-// import { data } from "../entities/data";
-
-type ModalProps<T> = {
-  show: boolean;
-  entity?: T;
-};
+import Input from "../components/Input";
+import Layout from "../components/Layout";
+import Message from "../components/Message";
+import Modal from "../components/Modal";
+import { Client } from "../entities/Client";
+import {
+  ClientSchema,
+  ClientValues,
+  DEFAULT_CLIENT_VALUES,
+} from "../schemas/clientSchema";
+import clientService from "../services/clientService";
+import "../styles/HomePage.css";
+import { MessageType } from "../utils/types";
 
 const HomePage: React.FC = () => {
   const [clients, setClients] = useState<Client[]>([]);
-  const [modal, setModal] = useState<ModalProps<Client>>({ show: false });
+  const [showModal, setShowModal] = useState<boolean>(false);
   const [message, setMessage] = React.useState<MessageType>({
     type: null,
     text: null,
   });
-
-  const closeModal = () => setModal({ show: false });
-
-  const getAllClients = async () => {
-    const response = await clientService.getAll();
-    const { clients } = await response.json();
-    setClients(clients);
-  };
-
-  const initialValues: ClientValues = useMemo(
-    () => ({
-      firstname: "",
-      lastname: "",
-      phone: "",
-      email: "",
-      address: "",
-    }),
-    []
+  const [formInitialValues, setFormInitialValues] = useState<ClientValues>(
+    DEFAULT_CLIENT_VALUES
   );
 
   React.useEffect(() => {
@@ -61,105 +42,124 @@ const HomePage: React.FC = () => {
     }
   }, [message]);
 
+  const closeModal = () => setShowModal(false);
+
+  const getAllClients = async () => {
+    const response = await clientService.getAll();
+    const { clients } = await response.json();
+    setClients(clients);
+  };
+
+  const openModalToEdit = (clientId: number) => {
+    setFormInitialValues(
+      clients
+        .filter((client) => client.id === clientId)
+        .map((client) => {
+          delete client.userId;
+          return client;
+        })
+        .pop() as ClientValues
+    );
+    setShowModal(true);
+  };
+
   return (
     <Layout>
       <div className="home">
         <div className="container">
           <div className="header add-button">
             <h1>Clients</h1>
-            <button
-              className="primary"
-              onClick={() =>
-                setModal({
-                  show: true,
-                })
-              }
-            >
+            <button className="primary" onClick={() => setShowModal(true)}>
               <FaPlus />
               <span>Ajouter</span>
             </button>
           </div>
-          <ClientList clients={clients} />
-          {modal.show && (
-            <Modal
-              close={closeModal}
-              title="Ajouter un client"
-              entity={modal.entity}
-              form={
-                <Formik
-                  initialValues={initialValues}
-                  validateOnChange={false}
-                  validateOnBlur={false}
-                  validationSchema={ClientSchema}
-                  onSubmit={async (values, { setErrors }) => {
-                    const response = await clientService.create(values);
+          <ClientList clients={clients} editEvent={openModalToEdit} />
+          {showModal && (
+            <Modal close={closeModal} title="Ajouter un client">
+              <Formik
+                initialValues={formInitialValues}
+                validateOnChange={false}
+                validateOnBlur={false}
+                validationSchema={ClientSchema}
+                onSubmit={async (values) => {
+                  const response = formInitialValues.id
+                    ? await clientService.edit(values)
+                    : await clientService.create(values);
 
-                    if (response.ok) {
-                      getAllClients();
-                      closeModal();
-                    }
-                  }}
-                >
-                  {({ values, handleChange, handleSubmit, errors }) => (
-                    <form onSubmit={handleSubmit}>
-                      {message.type && (
-                        <Message type={message.type}>{message.text}</Message>
-                      )}
-                      <Input
-                        id="firstname"
-                        label="Prénom"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.firstname}
-                        error={errors.firstname}
-                        isRequired
-                      />
-                      <Input
-                        id="lastname"
-                        label="Nom"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.lastname}
-                        error={errors.lastname}
-                        isRequired
-                      />
-                      <Input
-                        id="email"
-                        label="Email"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.email}
-                        error={errors.email}
-                        isRequired
-                      />
-                      <Input
-                        id="phone"
-                        label="Téléphone"
-                        type="text"
-                        onChange={handleChange}
-                        value={values.phone}
-                        error={errors.phone}
-                        isRequired
-                      />
-                      <Input
-                        isTextArea
-                        id="address"
-                        label="Adresse"
-                        onChange={handleChange}
-                        value={values.address}
-                        error={errors.address}
-                        isRequired
-                      />
-                      <div className="center">
-                        <button className="primary" type="submit">
-                          Ajouter
-                        </button>
-                      </div>
-                    </form>
-                  )}
-                </Formik>
-              }
-            />
+                  if (response.ok) {
+                    getAllClients();
+                    closeModal();
+                  }
+                }}
+              >
+                {({ values, handleChange, handleSubmit, errors }) => (
+                  <form onSubmit={handleSubmit}>
+                    {message.type && (
+                      <Message type={message.type}>{message.text}</Message>
+                    )}
+                    <Input
+                      id="firstname"
+                      label="Prénom"
+                      type="text"
+                      onChange={handleChange}
+                      value={values.firstname}
+                      error={errors.firstname}
+                      isRequired
+                    />
+                    <Input
+                      id="lastname"
+                      label="Nom"
+                      type="text"
+                      onChange={handleChange}
+                      value={values.lastname}
+                      error={errors.lastname}
+                      isRequired
+                    />
+                    <Input
+                      id="email"
+                      label="Email"
+                      type="text"
+                      onChange={handleChange}
+                      value={values.email}
+                      error={errors.email}
+                      isRequired
+                    />
+                    <Input
+                      id="phone"
+                      label="Téléphone"
+                      type="text"
+                      onChange={handleChange}
+                      value={values.phone}
+                      error={errors.phone}
+                      isRequired
+                    />
+                    <Input
+                      isTextArea
+                      id="address"
+                      label="Adresse"
+                      onChange={handleChange}
+                      value={values.address}
+                      error={errors.address}
+                      isRequired
+                    />
+                    <div className="center">
+                      <button
+                        className="primary"
+                        type="submit"
+                        disabled={
+                          formInitialValues.id !== undefined &&
+                          JSON.stringify(values) ===
+                            JSON.stringify(formInitialValues)
+                        }
+                      >
+                        {formInitialValues.id ? "Ajouter" : "Modifier"}
+                      </button>
+                    </div>
+                  </form>
+                )}
+              </Formik>
+            </Modal>
           )}
         </div>
       </div>
