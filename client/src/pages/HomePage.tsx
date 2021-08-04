4903f55baf91365476daa2f4e2339a8c19eefcd5
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-04 15:38:59 +0200

    add client deletion

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Formik } from "formik";
 import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import Alert from "../components/Alert";
 import ClientList from "../components/ClientList";
 import Input from "../components/Input";
 import Layout from "../components/Layout";
@@ -20,6 +21,7 @@ import { translateMessage } from "../utils/responseMessage";
 const HomePage: React.FC = () => {
   const [clients, setClients] = useState<Client[]>([]);
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [alert, setAlert] = useState<number | undefined>();
   const [formInitialValues, setFormInitialValues] = useState<ClientValues>(
     DEFAULT_CLIENT_VALUES
   );
@@ -31,23 +33,47 @@ const HomePage: React.FC = () => {
 
   const closeModal = () => setShowModal(false);
 
+  const closeAlert = () => setAlert(undefined);
+
   const getAllClients = async () => {
     const response = await clientService.getAll();
     const { clients } = await response.json();
     setClients(clients);
   };
 
-  const openModalToEdit = (clientId: number) => {
-    setFormInitialValues(
-      clients
-        .filter((client) => client.id === clientId)
-        .map((client) => {
-          delete client.userId;
-          return client;
-        })
-        .pop() as ClientValues
-    );
-    setShowModal(true);
+  const openModalToEdit = (clientId: number | undefined) => {
+    if (clientId) {
+      setFormInitialValues(
+        clients
+          .filter((client) => client.id === clientId)
+          .map((client) => {
+            delete client.userId;
+            return client;
+          })
+          .pop() as ClientValues
+      );
+      setShowModal(true);
+    }
+  };
+
+  const getClientFullName = (client: Client | undefined) =>
+    client ? client.firstname + " " + client.lastname : null;
+
+  const openModalToDelete = (clientId: number | undefined) => {
+    if (clientId) {
+      setAlert(clientId);
+    }
+  };
+
+  const deleteClient = async () => {
+    if (alert) {
+      const response = await clientService.remove(alert);
+
+      if (response.ok) {
+        getAllClients();
+        closeAlert();
+      }
+    }
   };
 
   return (
@@ -61,7 +87,23 @@ const HomePage: React.FC = () => {
               <span>Ajouter</span>
             </button>
           </div>
-          <ClientList clients={clients} editEvent={openModalToEdit} />
+          <ClientList
+            clients={clients}
+            editEvent={openModalToEdit}
+            deleteEvent={openModalToDelete}
+          />
+          {alert && (
+            <Alert
+              title="Supprimer client"
+              text={`Voulez-vous vraiment supprimez votre client : ${getClientFullName(
+                clients.filter((client) => client.id === alert).pop()
+              )} ?`}
+              type="delete"
+              buttonName="Supprimer"
+              close={closeAlert}
+              confirmEvent={deleteClient}
+            />
+          )}
           {showModal && (
             <Modal close={closeModal} title="Ajouter un client">
               <Formik
