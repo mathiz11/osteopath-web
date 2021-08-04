791d309e85c6559c3a84416cd16005e6bbf567dc
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-04 15:37:24 +0200

    add alert component

---
+++ client/src/components/Alert.tsx
@@ -0,0 +1,38 @@
+import "../styles/Alert.css";
+
+type AlertProps = {
+  title: string;
+  text: string;
+  type: "delete";
+  buttonName: string;
+  close: () => void;
+  confirmEvent: () => void;
+};
+
+const Alert = ({
+  title,
+  text,
+  type,
+  buttonName,
+  close,
+  confirmEvent,
+}: AlertProps) => {
+  return (
+    <div className="alert-container" onClick={close}>
+      <div className="alert">
+        <div className="header">{title}</div>
+        <div className="content">{text}</div>
+        <div className="footer">
+          <button className="secondary" onClick={close}>
+            Annuler
+          </button>
+          <button className={`primary ${type}`} onClick={confirmEvent}>
+            {buttonName}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
