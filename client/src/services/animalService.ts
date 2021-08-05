b6755055625fa76f9a4e6130b17106337e5eecdb
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-05 15:34:54 +0200

    add create, update and delete animal services

---
+++ client/src/services/animalService.ts
@@ -0,0 +1,73 @@
+import { AnimalType } from "../entities/Animal";
+import { AnimalValues } from "../schemas/animalSchema";
+import { fetchManager } from "./globalService";
+
+const checkAnimalValues = (animal: AnimalValues) => {
+  if (animal.type !== AnimalType.NAC && animal.subtype) {
+    animal.subtype = null;
+  }
+  return animal;
+};
+
+const create = async (
+  clientId: number,
+  animal: AnimalValues
+): Promise<Response> => {
+  return fetchManager({
+    url: process.env.REACT_APP_API_URL + "/clients/" + clientId + "/animals",
+    options: {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+    body: JSON.stringify(checkAnimalValues(animal)),
+  });
+};
+
+const edit = async (
+  clientId: number,
+  animal: AnimalValues
+): Promise<Response> => {
+  return fetchManager({
+    url: process.env.REACT_APP_API_URL + "/clients/" + clientId + "/animals",
+    options: {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+    body: JSON.stringify(checkAnimalValues(animal)),
+  });
+};
+
+const remove = async (
+  clientId: number,
+  animalId: number
+): Promise<Response> => {
+  return fetchManager({
+    url:
+      process.env.REACT_APP_API_URL +
+      "/clients/" +
+      clientId +
+      "/animals/" +
+      animalId,
+    options: {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  });
+};
+
+const animalService = {
+  create,
+  edit,
+  remove,
+};
+
+export default animalService;
