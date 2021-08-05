5b9cb627fbd471709179ae38b05e9fef0661b913
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-05 15:33:33 +0200

    add select and possibility to disable an input

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,4 +1,6 @@
 import { ChangeEvent } from "react";
+import { BsChevronDown } from "react-icons/bs";
+import { AnimalSubtype, AnimalType } from "../entities/Animal";
 import "../styles/Input.css";
 
 type InputProps = {
@@ -11,6 +13,8 @@ type InputProps = {
   error?: string;
   isRequired?: boolean;
   isTextArea?: boolean;
+  select?: typeof AnimalType | typeof AnimalSubtype;
+  isDisabled?: boolean;
 };
 
 const Input = ({
@@ -23,19 +27,47 @@ const Input = ({
   error,
   isRequired,
   isTextArea,
+  select,
+  isDisabled,
 }: InputProps) => {
   return (
     <div className={`form-control${error ? " error" : ""}`}>
       <label htmlFor={id}>{`${label}${isRequired ? " *" : ""}`}</label>
-      {isTextArea ? (
-        <textarea id={id} onChange={onChange} value={value} rows={3} />
-      ) : (
+      {isTextArea && (
+        <textarea
+          id={id}
+          onChange={onChange}
+          value={value}
+          rows={3}
+          disabled={isDisabled}
+        />
+      )}
+      {select && (
+        <div className="select">
+          <select
+            id={id}
+            onChange={onChange}
+            value={value}
+            disabled={isDisabled}
+          >
+            <option value=""></option>
+            {Object.values(select).map((option, i) => (
+              <option key={id + i} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <BsChevronDown size="14" />
+        </div>
+      )}
+      {!isTextArea && !select && (
         <input
           id={id}
           placeholder={placeholder}
           type={type ? type : "text"}
           value={value}
           onChange={onChange}
+          disabled={isDisabled}
         />
       )}
       <span className="form-error">{error}</span>
