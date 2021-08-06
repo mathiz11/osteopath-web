d5d1630c28ffbbfe7c8819756a5c519b2476b75a
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-06 11:21:52 +0200

    add edit route for user

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { User } from "../entities/User";
 import { isAuth } from "../middlewares/isAuth";
 import { getConnection } from "typeorm";
+import { body } from "express-validator";
+import { checkErrors } from "../middlewares/checkErrors";
 
 const router = express.Router();
 
@@ -22,5 +24,48 @@ router.get("/", isAuth, async (req, res) => {
 
   res.json({ user });
 });
+
+router.put(
+  "/",
+  isAuth,
+  body("id").isInt(),
+  body("firstname").notEmpty().isLength({ max: 100 }),
+  body("lastname").notEmpty().isLength({ max: 100 }),
+  body("email").isEmail().isLength({ max: 100 }),
+  body("numberOA")
+    .default(null)
+    .optional({ nullable: true })
+    .isLength({ max: 30 }),
+  body("phone")
+    .default(null)
+    .optional({ nullable: true })
+    .isLength({ min: 10, max: 10 }),
+  body("address").default(null).optional({ nullable: true }).isString(),
+  checkErrors,
+  async (req, res) => {
+    const { id, firstname, lastname, email, numberOA, phone, address } =
+      req.body;
+
+    try {
+      await getConnection()
+        .createQueryBuilder()
+        .update(User)
+        .set({
+          firstname,
+          lastname,
+          email,
+          numberOA,
+          phone,
+          address,
+        })
+        .where("id = :id", { id })
+        .execute();
+
+      res.json({ message: "user updated" });
+    } catch (e) {
+      res.status(500).json({ message: "user update error" });
+    }
+  }
+);
 
 export default router;
