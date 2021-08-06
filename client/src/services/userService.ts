9999bea475f811103be733fd7572c51e8733450a
Mathis Enjolras <mathis.enjolras@mines-ales.org>
2021-08-06 11:21:22 +0200

    add edit in user service

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,3 +1,4 @@
+import { UserValues } from "../schemas/userSchema";
 import { fetchManager } from "./globalService";
 
 const get = () => {
@@ -13,8 +14,23 @@ const get = () => {
   });
 };
 
+const edit = async (user: UserValues): Promise<Response> => {
+  return fetchManager({
+    url: process.env.REACT_APP_API_URL + "/users",
+    options: {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+    body: JSON.stringify(user),
+  });
+};
+
 const userService = {
   get,
+  edit,
 };
 
 export default userService;
